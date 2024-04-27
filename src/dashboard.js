95f0fde768b5a6758c0616af4dc5f7e3f9f76f32
Shaneel <2539340@students.wits.ac.za>
2024-04-27 13:35:11 +0200

    added dashboard

---
+++ src/dashboard.js
@@ -0,0 +1,4 @@
+// Retrieve data from localStorage
+const username = localStorage.getItem('username');
+const role = localStorage.getItem('role');
+document.getElementById('username').textContent = username;
